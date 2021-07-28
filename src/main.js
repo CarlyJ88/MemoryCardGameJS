5480965b3bcf5a875d9a67bdbf603ef5872a672a
Carly <carly.jenkinson65@googlemail.com>
2021-07-28 17:19:32 +0100

    Can flip cards.

--- src/main.js
+++ src/main.js
@@ -1,29 +1,39 @@
-function MemoryCardGame(){
+function MemoryCardGame() {
   createTable();
+  var cards = document.getElementsByClassName('card');
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', function (e) {
+      e.currentTarget.classList.toggle('is-flipped');
+    }, false);
+  }
 }
 MemoryCardGame();
-
-function renderCellText(cellContent) {
-  let newCell1 = document.createElement('td');
-  newCell1.innerText = cellContent;
-  return newCell1;
-}
 
 function createTable() {
   const table = document.getElementById('cards');
 
-  for (let i = 0; i < 6; i++){
+  for (let i = 0; i < 6; i++) {
     let newRow = document.createElement('tr');
-    for (var j = 0; j < 5; j++) {
-        var td = document.createElement('td');
-        let div = document.createElement('div');
-        div.id = 'back';
-        div.className = 'card';
-        td.append(div);
-        newRow.appendChild(td)
+    for (var j = 0; j < 6; j++) {
+      var td = document.createElement('td');
+      td.className = 'scene';
+
+      let div = document.createElement('div');
+      div.className = 'card'
+      div.append(createDiv('front', 6 * i + j))
+      div.append(createDiv('back', 6 * i + j))
+
+      td.append(div);
+
+      newRow.appendChild(td)
     }
-    let div = document.createElement('div');
-    newRow.append(div);
     table.append(newRow);
   }
+}
+
+function createDiv(side, id) {
+  let div = document.createElement('div');
+  div.id = side + id;
+  div.className = 'card__face card__face--' + side;
+  return div
 }
