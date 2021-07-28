9535ef3f5b881d6bfaa4840f2f80fca1035d2dc2
Carly <carly.jenkinson65@googlemail.com>
2021-07-28 21:20:34 +0100

    Show image when card is flipped.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 function MemoryCardGame() {
-  createTable();
+  createTable(getImage());
   var cards = document.getElementsByClassName('card');
   for (var i = 0; i < cards.length; i++) {
     cards[i].addEventListener('click', function (e) {
@@ -9,7 +9,7 @@ function MemoryCardGame() {
 }
 MemoryCardGame();
 
-function createTable() {
+function createTable(images) {
   const table = document.getElementById('cards');
 
   for (let i = 0; i < 6; i++) {
@@ -21,7 +21,7 @@ function createTable() {
       let div = document.createElement('div');
       div.className = 'card'
       div.append(createDiv('front', 6 * i + j))
-      div.append(createDiv('back', 6 * i + j))
+      div.append(createDiv('back', 6 * i + j, images))
 
       td.append(div);
 
@@ -31,9 +31,27 @@ function createTable() {
   }
 }
 
-function createDiv(side, id) {
+function createDiv(side, id, images) {
   let div = document.createElement('div');
   div.id = side + id;
   div.className = 'card__face card__face--' + side;
+
+  if (side === 'back'){
+    let image = document.createElement("img");
+    // for (let i = 0; i < images.length; i++){
+      image.src = images[id];
+    // }
+    div.appendChild(image);
+  }
   return div
+}
+
+function getImage(){
+  let images = ["images/air.png", "images/copy.jpeg", "images/cylindar.png", "images/earth.png", "images/fire.png", "images/infinity.jpeg",
+   "images/monitor.png", "images/moon.png", "images/noentry.png", "images/paperclip.png", "images/recycle.png", "images/ruby.png", 
+   "images/star.png", "images/sun.png", "images/toxic.png", "images/unity.png", "images/water.png", "images/yinyang.png"]
+   images = images.concat(images);
+   images.sort(() => Math.random() - 0.5)
+   console.log(images, 'images')
+   return images;
 }
